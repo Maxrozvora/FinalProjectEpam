1875de8466ca2a86dac6f457cb5f06f257364474
Maxrozvora <rozvora.max@gmail.com>
2019-05-13 21:31:14 +0300

    work (PostItem.vue item.sass) Реализация метода лайка

--- photo-lib/src/components/PostItem.vue
+++ photo-lib/src/components/PostItem.vue
@@ -4,11 +4,18 @@
                 <img :src="post.userImage" :alt="post.username" class="post__avatar">
                 <span class="post__author-name">{{post.username}}</span>
             </div>
-            <div class="post__image" :style="{ backgroundImage: 'url(' + post.postImage + ')' }">
+            <div class="post__image"
+                 :class="post.filter"
+                 :style="{ backgroundImage: 'url(' + post.postImage + ')' }"
+                 @dblclick="like"
+            >
             </div>
             <div class="post__content">
                 <div class="post__heart">
-                    <i class="fa fa-heart-o"></i>
+                    <i class="fa"
+                       :class="[this.post.hasBeenLiked ? 'fa-heart' : 'fa-heart-o']"
+                    @click="like"
+></i>
                     <span class="post__likes">{{post.likes}} подобається</span>
                 </div>
                 <span class="post__author-name">{{post.username}}</span>
@@ -22,6 +29,14 @@
         name: "PostItem",
         props: {
             post: Object
+        },
+        methods: {
+            like () {
+                this.post.hasBeenLiked
+                ? this.post.likes--
+                : this.post.likes++;
+                this.post.hasBeenLiked = !this.post.hasBeenLiked;
+            }
         }
     }
 </script>
