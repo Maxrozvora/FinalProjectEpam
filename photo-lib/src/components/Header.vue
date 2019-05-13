81b328efd0ff84615ba88d43bbcc29570cf41857
Maxrozvora <rozvora.max@gmail.com>
2019-05-13 22:05:56 +0300

    style (Header.vue _media.sass header.sass main.sass) Добавление стилей для header и удаление файла с медиазапросами

--- photo-lib/src/components/Header.vue
+++ photo-lib/src/components/Header.vue
@@ -1,5 +1,43 @@
 <template>
-    <h1>Header</h1>
+    <header class="top-line">
+        <a href="/" class="logo logo-litera">PhotoTOP</a>
+        <div class="search-wrap">
+            <div class="search" title="Поиск по сату">
+                <div class="fa fa-search"></div>
+            </div>
+            <div class="search-field">
+                <form action="/search.html" id="cse-search-box">
+                    <input type="hidden" name="cx" value="partner-pub-xxx:xxx"/>
+                    <input type="hidden" name="cof" value="FORID:10"/>
+                    <input type="hidden" name="ie" value="UTF-8"/>
+                    <input type="text" name="q" placeholder="Поиск по сайту..."/>
+                </form>
+            </div>
+        </div>
+        <div class="mobile-menu-button"><i class="fa fa-bars"></i> Меню</div>
+        <nav class="main-menu top-menu">
+            <ul class="main-menu__list">
+                <li class="main-menu__item active"><a href="#" class="main-menu__link">Головна</a></li>
+                <li class="main-menu__item"><a href="#" class="main-menu__link">Контакти</a></li>
+                <li class="main-menu__item"><a href="#" class="main-menu__link">Добавити фото</a></li>
+                <li class="main-menu__item">
+                    <a href="#" class="main-menu__link">Профіль</a>
+                    <ul class="main-menu__sub-menu">
+                        <li class="main-menu__item-sub">
+                            <a href="#" class="main-menu__link">Вхід</a>
+                        </li>
+                        <li class="main-menu__item-sub">
+                            <a href="#" class="main-menu__link">Реєстрація</a>
+                        </li>
+                        <li class="main-menu__item-sub">
+                            <a href="#" class="main-menu__link">Вихід</a>
+                        </li>
+                    </ul>
+                </li>
+
+            </ul>
+        </nav>
+    </header>
 </template>
 
 <script>
@@ -8,6 +46,6 @@
     }
 </script>
 
-<style scoped>
+<style scoped lang="sass" src="../styles/sass/header.sass">
 
 </style>
