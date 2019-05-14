8dc1581775458df88b8cc8325d7b0abfecef18c5
Maxrozvora <rozvora.max@gmail.com>
2019-05-14 21:23:22 +0300

    work (Profile.vue profile.sass) Стилизация профиля

--- photo-lib/src/components/Profile.vue
+++ photo-lib/src/components/Profile.vue
@@ -1,16 +1,19 @@
 <template>
     <div class="profile">
-        <div class="profile__top">
-            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1211695/me_3.jpg" alt="Max Rozvora" class="profile__img">
-            <div class="profile__count">
-                <div class="profile__number">8</div>
-                <div class="profile__subtitle">Дописів</div>
+        <div class="profile__content">
+            <div>
+                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1211695/me_3.jpg" alt="Max Rozvora" class="profile__img">
             </div>
+                <div class="profile__descr">
+                    <div class="profile__number">8</div>
+                    <div class="profile__subtitle">Дописів</div>
+                    <div class="profile__name"><span class="profile__subtitle">Логін: </span>Max Rozvora</div>
+                    <div class="profile__email"><span class="profile__subtitle">Email: </span>max.rozvora@gmail.com</div>
+                </div>
         </div>
-        <div class="profile__bottom">
-            <div class="profile__name">Max Rozvora</div>
-            <div class="profile__email">max.rozvora@gmail.com</div>
-        </div>
+
+
+
     </div>
 </template>
 
