d27285fedda229c4d388594e67cdf2fefdb34449
Maxrozvora <rozvora.max@gmail.com>
2019-05-14 16:40:48 +0300

    work (Slider.vue About.vue Home.vue) Вывод слайдера

---
+++ photo-lib/src/components/Slider.vue
@@ -0,0 +1,28 @@
+<template>
+    <hooper :itemsToShow="4" :infiniteScroll="true">
+        <slide v-for="(post, index) in posts" :key="index">
+            <img :src="post.postImage" :alt="post.caption">
+        </slide>
+    </hooper>
+</template>
+
+<script>
+    import { Hooper, Slide } from 'hooper';
+    import 'hooper/dist/hooper.css';
+
+    export default {
+        name: 'Slider',
+        props: {
+            posts: Array
+        },
+        components: {
+            Hooper,
+            Slide
+        }
+    }
+</script>
+<style scoped>
+    .hooper {
+        height: auto;
+    }
+</style>
--- photo-lib/src/views/Home.vue
+++ photo-lib/src/views/Home.vue
@@ -1,32 +1,37 @@
 <template>
-  <div class="home">
+    <div class="home">
+        <slider-item
+            :posts="posts"
+        ></slider-item>
         <div class="post-wrapper">
-      <post-item
-              v-for="(post, index) in posts" :key="index"
-        :post="post"
-        :filter="filters[index]"
-      ></post-item>
+            <post-item
+                    v-for="(post, index) in posts" :key="index"
+                    :post="post"
+                    :filter="filters[index]"
+            ></post-item>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import PostItem from '@/components/PostItem'
+    // @ is an alias to /src
+    import PostItem from '@/components/PostItem'
+    import Slider from '@/components/Slider'
 
-import posts from "../data/posts"
-import filters from "../data/filters"
+    import posts from "../data/posts"
+    import filters from "../data/filters"
 
-export default {
-  name: 'home',
-  data () {
-    return {
-      posts,
-      filters,
+    export default {
+        name: 'home',
+        data() {
+            return {
+                posts,
+                filters,
+            }
+        },
+        components: {
+            'post-item': PostItem,
+            'slider-item': Slider
+        }
     }
-  },
-  components: {
-    'post-item': PostItem
-  }
-}
 </script>
