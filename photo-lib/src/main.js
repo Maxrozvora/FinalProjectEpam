7cb08508e7289beebf78d87d2b5ea440d7b0a37d
Maxrozvora <rozvora.max@gmail.com>
2019-05-15 20:13:43 +0300

    add (main.js  package.json yarn.lock) Подключение firebase

--- photo-lib/src/main.js
+++ photo-lib/src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import Vuelidate from 'vuelidate'
+import * as firebase from 'firebase'
 import store from './store'
 
 Vue.use(Vuelidate)
@@ -10,5 +11,17 @@ Vue.config.productionTip = false
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: h => h(App),
+  created() {
+    const firebaseConfig = {
+      apiKey: 'AIzaSyBbZuwBYe_dLlGaC03khc-u64VRbAIV_6A',
+      authDomain: 'phototop-14ccd.firebaseapp.com',
+      databaseURL: 'https://phototop-14ccd.firebaseio.com',
+      projectId: 'phototop-14ccd',
+      storageBucket: 'phototop-14ccd.appspot.com',
+      messagingSenderId: '853675311253',
+      appId: '1:853675311253:web:7457b38c4e26734b'
+    };
+    firebase.initializeApp(firebaseConfig);
+}
 }).$mount('#app')
