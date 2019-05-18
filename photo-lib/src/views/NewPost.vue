6c540379c4078f50eb36277e549677f17377c41b
Maxrozvora <rozvora.max@gmail.com>
2019-05-18 12:13:37 +0300

    work (form.sass NewPost.vue) Добавление логики для загрузки картинки

--- photo-lib/src/views/NewPost.vue
+++ photo-lib/src/views/NewPost.vue
@@ -1,11 +1,12 @@
 <template>
     <form class="form" @submit.prevent="onSubmit">
         <div class="input-group">
+            <img class="image" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1211695/me_3.jpg">
             <label for="image">Фото</label>
             <input
-                    @change="image"
+                    v-model="image"
                     @blur="$v.image.$touch()"
-                    type="file"
+                    type="text"
                     class="input"
                     :class="{'invalid': $v.image.$error}"
                     id="image"
@@ -37,8 +38,17 @@
         data () {
             return {
                 image: '',
-                description: '',
-                like: 0,
+                description: ''
+            }
+        },
+        methods: {
+            onSubmit () {
+                const newPost = {
+                    image: this.image,
+                    description: this.description,
+                    likes: 0
+                }
+                // console.log(newPost); // TODO console.log
             }
         },
         validations: {
