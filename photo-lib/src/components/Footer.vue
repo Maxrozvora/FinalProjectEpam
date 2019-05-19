ff07a9699ec11a06fc0bf49360d6aab267ecb56d
Maxrozvora <rozvora.max@gmail.com>
2019-05-19 11:28:12 +0300

    work (Footer.vue footer.sass) Добавление верстики и стилей для футера

--- photo-lib/src/components/Footer.vue
+++ photo-lib/src/components/Footer.vue
@@ -1,5 +1,45 @@
 <template>
-    <h1>Footer</h1>
+    <div>
+        <footer class="main-footer">
+
+                <nav class="main-menu footer-menu">
+                    <ul>
+                        <li class="active"><a href="#">Электроника</a></li>
+                        <li><a href="#">Новости IT</a></li>
+                        <li><a href="#">Обзоры</a></li>
+                        <li><a href="#">Лайфхаки</a></li>
+                        <li><a href="#">DIY</a></li>
+                        <li><a href="#">О Сайте</a></li>
+                    </ul>
+                </nav>
+                <nav class="soc">
+                    <ul>
+                        <li>
+                            <a href="#"><i class="fa fa-vk"></i></a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="fa fa-facebook"></i></a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="fa fa-instagram"></i></a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="fa fa-odnoklassniki"></i></a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="fa fa-rss"></i></a>
+                        </li>
+                    </ul>
+                </nav>
+
+
+        </footer>
+        <footer class="bottom-footer">
+            <div class="text-center">
+                <div class="footer-copy">2019 © <span class="logo-footer logo-litera">PhotoTop</span></div>
+            </div>
+        </footer>
+    </div>
 </template>
 
 <script>
@@ -8,6 +48,6 @@
     }
 </script>
 
-<style scoped>
+<style scoped lang="sass" src="../styles/sass/footer.sass">
 
 </style>
